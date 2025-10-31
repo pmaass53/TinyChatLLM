8c4cfa646b30555e4698fd01fc9146cfa0005d33
pmaass53 <pippinmaass53@gmail.com>
2025-10-31 13:17:57 +0000

    modify train.py

---
+++ train.py
@@ -0,0 +1,19 @@
+import os
+import math
+import time
+import random
+
+vocab = []
+token_to_id = []
+id_to_token = []
+sample_text = ""
+
+# Load Tokens
+with open("tokens.txt", "r", encoding="utf-8") as f:
+    vocab = [t.strip() for t in f.readlines()]
+    token_to_id = {t: i for i, t in enumerate(vocab)}
+    id_to_token = {i: t for t, i in token_to_id.items()}
+
+# Load Conversation
+with open("tinychat.txt", "r", encoding="utf-8") as f:
+    sample_text = f.read()
